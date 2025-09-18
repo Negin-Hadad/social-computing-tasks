081f1f41926c11a9c3d815eb44248234bd99fc5b
negin.haddad <negin.haddad@partsoftware.com>
2025-09-18 20:39:42 +0330

    add task 1.2 answer

--- HW1.py
+++ HW1.py
@@ -22,5 +22,17 @@ for table in ['users', 'posts', 'comments', 'reactions', 'follows']:
     print(columns[["name", "type"]])
     print("-------------------------------")
     
+# Task 1.2
+try:
+    notReactedOrPostedUsers = pd.read_sql_query("""
+                            SELECT id FROM users
+                                WHERE users.id NOT IN
+                                    (SELECT DISTINCT(reactions.user_id) FROM reactions
+                                    UNION
+                                    SELECT DISTINCT(posts.user_id) FROM posts);
+                        """, conn)
+    print(len(notReactedOrPostedUsers))
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
 conn.close()
