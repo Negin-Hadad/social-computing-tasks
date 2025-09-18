2fcfa11b2c1a0f10a9cf74da00d27e3ad477640e
negin.haddad <negin.haddad@partsoftware.com>
2025-09-18 19:36:16 +0330

    add task 1.1 answer

--- HW1.py
+++ HW1.py
@@ -1,3 +1,26 @@
 # pip install pandas
 import sqlite3
 import pandas as pd
+
+DB_FILE = r"e:\OuluUni\period1\SocialComputing\assignments\database.sqlite"
+
+try:
+    conn = sqlite3.connect(DB_FILE)
+    print("SQLite DB connection successful")
+except Exception as e:
+    print(f"Error in connecting to DB: '{e}'")
+
+# Task 1.1
+tableNames_df = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table'", conn)
+print(f'table names:\n {tableNames_df}')
+
+for table in ['users', 'posts', 'comments', 'reactions', 'follows']:
+    tableContent = pd.read_sql_query(f"SELECT * FROM {table}", conn)
+    print(f'\nTable "{table}" number of rows: {len(tableContent)}')
+    print(f'\nTable "{table}" columns description:')
+    columns = pd.read_sql_query(f"PRAGMA table_info({table})", conn)
+    print(columns[["name", "type"]])
+    print("-------------------------------")
+    
+
+conn.close()
